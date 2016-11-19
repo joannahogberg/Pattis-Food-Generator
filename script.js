eb3ffbf19cb9fe210ecf0619bd9a15a045af21eb
Joanna Högberg <joannahogberg@users.noreply.github.com>
2016-11-19 22:12:55 +0100

    array update

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 //Array med objekt för olika maträtter
 var meals = [
-    { name: "Fajitas", veg: false, url: "http://www.ica.se/recept/original-fajitas-718669/", img: "pics/fajitas.jpg" },
-    { name: "Fajitas Veggie Style", veg: true, url: "http://www.ica.se/recept/fajitas-med-sojafars-och-bonsalsa-719808/", img: "pics/fajitas_veggie.jpg" },
-    { name: "Chili Con Carne", veg: false, url: "http://www.ica.se/recept/chili-con-carne-424/", img: "pics/chiliconcarne.jpg" },
-    { name: "Chili Sin Carne", veg: true, url: "http://www.koket.se/gert_klotzke/soppor_och_grytor/artor__bonor_och_linser/chili_sin_carne/", img: "pics/chili_sin_carne.jpg" },
-    { name: "Pulled Beef Burger", veg: false, url: "http://www.tasteline.com/recept/pulled-beef-burgare-med-hemgjorda-pommes-frites-och-inlagd-rodlok/", img: "pics/pulledbeefburger.jpg" },
-    { name: "Vegetarian Pulled Pork Burger", veg: true, url: "http://gronaskafferiet.se/vegetarisk-pulled-pork-burgare-med-coleslaw/", img: "pics/veggiepulled_pork.jpg" },
-    { name: "Moule Frites with Aioli", veg: false, url: "http://www.koket.se/koket/anders-leven/moules-frites-med-aioli/", img: "pics/moules.jpg" },
-    { name: "Chevre Chaud", veg: true, url: "http://www.tasteline.com/recept/chevre-chaud-gratinerad-getost/", img: "pics/chevre_chaud.jpg" },
-    { name: "Pasta Vongole", veg: false, url: "http://www.koket.se/per-morberg/varmratter/pasta-och-nudlar/per-morbergs-pasta-vongole/", img: "pics/pastavongole.jpg" },
-    { name: "Indian Daal", veg: true, url: "http://www.tasteline.com/recept/daal/", img: "pics/daal.jpg" },
-    { name: "Chicken Tikka Masala", veg: false, url: "http://marcussamuelsson.se/recept/kyckling-tikka-masala-recept/", img: "pics/tikkamasala.jpg" },
-    { name: "Potatoe Leek Soup", veg: true, url: "http://www.koket.se/filip-fasten/soppor-grytor/gronsaker-potatis-rotfrukter/potatis--och-purjolokssoppa/", img: "pics/potatissoppa.jpg" },
-    { name: "Beef Rydberg", veg: false, url: "http://www.koket.se/per_morberg/varmratter/kott/biff_rydberg_a_la_morberg/", img: "pics/biffrydberg.jpg" },
-    { name: "Carrot and Coconut Soup", veg: true, url: "http://www.hemtrevligt.se/icakuriren/recept/morotssoppa-med-kokosmjolk", img: "pics/carrotsoup.jpg" }
+    { name: "FAJITAS", veg: false, url: "http://www.ica.se/recept/original-fajitas-718669/", img: "pics/fajitas.jpg" },
+    { name: "FAJITAS VEGGIE STYLE", veg: true, url: "http://www.ica.se/recept/fajitas-med-sojafars-och-bonsalsa-719808/", img: "pics/fajitas_veggie.jpg" },
+    { name: "CHILI CON CARNE", veg: false, url: "http://www.ica.se/recept/chili-con-carne-424/", img: "pics/chiliconcarne.jpg" },
+    { name: "CHILI SIN CARNE", veg: true, url: "http://www.koket.se/gert_klotzke/soppor_och_grytor/artor__bonor_och_linser/chili_sin_carne/", img: "pics/chili_sin_carne.jpg" },
+    { name: "PULLED BEEF BURGER", veg: false, url: "http://www.tasteline.com/recept/pulled-beef-burgare-med-hemgjorda-pommes-frites-och-inlagd-rodlok/", img: "pics/pulledbeefburger.jpg" },
+    { name: "VEGGIE PULLED BEEF BURGER", veg: true, url: "http://gronaskafferiet.se/vegetarisk-pulled-pork-burgare-med-coleslaw/", img: "pics/veggiepulled_pork.jpg" },
+    { name: "MOULE FRITES WITH AIOLI", veg: false, url: "http://www.koket.se/koket/anders-leven/moules-frites-med-aioli/", img: "pics/moules.jpg" },
+    { name: "CHEVRE CHAUD", veg: true, url: "http://www.tasteline.com/recept/chevre-chaud-gratinerad-getost/", img: "pics/chevre_chaud.jpg" },
+    { name: "PASTA VONGOLE", veg: false, url: "http://www.koket.se/per-morberg/varmratter/pasta-och-nudlar/per-morbergs-pasta-vongole/", img: "pics/pastavongole.jpg" },
+    { name: "INDIAN DAAL", veg: true, url: "http://www.tasteline.com/recept/daal/", img: "pics/daal.jpg" },
+    { name: "CHICKEN TIKKA MASALA", veg: false, url: "http://marcussamuelsson.se/recept/kyckling-tikka-masala-recept/", img: "pics/tikkamasala.jpg" },
+    { name: "POTATOE LEEK SOUP", veg: true, url: "http://www.koket.se/filip-fasten/soppor-grytor/gronsaker-potatis-rotfrukter/potatis--och-purjolokssoppa/", img: "pics/potatissoppa.jpg" },
+    { name: "BIFF RYDBERG", veg: false, url: "http://www.koket.se/per_morberg/varmratter/kott/biff_rydberg_a_la_morberg/", img: "pics/biffrydberg.jpg" },
+    { name: "CARROT & COCONUT SOUP", veg: true, url: "http://www.hemtrevligt.se/icakuriren/recept/morotssoppa-med-kokosmjolk", img: "pics/carrotsoup.jpg" }
 ];
 
 //Knapp för att slumpmässigt ta fram recept
